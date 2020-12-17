a231809132bb438455ada19f780ed797d174270c
Charles Lana <charleslanop@gmail.com>
2020-12-17 20:58:36 -0300

    React 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 
 function App() {
+    const [counter, setCounter] = useState(1);
+
+    function handleButtonClick() {
+        setCounter(counter + 1);
+        console.log(counter);
+    }
     return (
         <>
             <Header title={'Página Inicial'}/>
             <Header title={'Outro'}/>
             <h1>Hello Dev</h1>
+            <p>{ counter }</p>
+            <button onClick={handleButtonClick}>Incrementar</button>
             <Menu>
                 <ul>
                     <li>Home</li>
