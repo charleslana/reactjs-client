f1fa379ecaf9e219966581581923da066f775a56
Charles Lana <charleslanop@gmail.com>
2020-12-17 22:14:30 -0300

    React 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import React, {useState} from 'react';
-import Header from "./components/Header";
-import Menu from "./components/Menu";
+import './App.css';
+import Routes from "./routes";
 
 function App() {
-    const [counter, setCounter] = useState(1);
-
-    function handleButtonClick() {
-        setCounter(counter + 1);
-        console.log(counter);
-    }
     return (
         <>
-            <Header title={'Página Inicial'}/>
-            <Header title={'Outro'}/>
-            <h1>Hello Dev</h1>
-            <p>{ counter }</p>
-            <button onClick={handleButtonClick}>Incrementar</button>
-            <Menu>
-                <ul>
-                    <li>Home</li>
-                    <li>Contact</li>
-                    <li>Abount</li>
-                </ul>
-            </Menu>
+            <Routes/>
         </>
     );
 }
---
+++ src/pages/CreateLocation/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home: React.FC = () => {
+    return (
+        <h1>Create Location</h1>
+    );
+}
+
+export default Home;
